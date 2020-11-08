ab840563c5df1f1ea808166486f7173943efbdef
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-11-08 15:18:49 +0530

    fe functionality done

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,9 +1,72 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Todo } from "./components/Todo";
+import * as styles from "./App.module.css";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [userInput, setUserInput] = useState("");
+
+  useEffect(() => {
+    const getNotes = async () => {
+      const response = await axios("/api/notes");
+      setNotes(response.data);
+    };
+
+    getNotes();
+  }, []);
+
+  const createTodo = async () => {
+    const {data: note} = await axios({
+      method: 'post',
+      url: '/api/notes',
+      data: {text: userInput}
+    });
+
+    setNotes([...notes, note]);
+  }
+
+  const toggleComplete = async (id) => {
+    const {data: note} = await axios({
+      method: 'patch',
+      url: `/api/notes/${id}`
+    });
+
+    const newNotes = notes.map((n) => n.id === id ? note : n);
+    setNotes(newNotes);
+  }
+
+  const deleteTodo = async (id) => {
+    await axios({method: 'delete', url: `/api/notes/${id}`});
+
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  }
+
   return (
-    <div className="App">
-      Boom
+    <div className={styles.app}>
+      <h2>Todos App</h2>
+      <div className={styles.inputGroup}>
+        <input
+          type="text"
+          placeholder="add a new todo"
+          className={styles.input}
+          onChange={e => setUserInput(e.target.value)}
+        />
+        <button onClick={createTodo} className={styles.button}>Go</button>
+      </div>
+      <div className={styles.notesList}>
+        {notes.map((note) => {
+          return <Todo
+            text={note.text}
+            completed={note.complete}
+            id={note.id}
+            key={note.id}
+            toggleComplete={toggleComplete}
+            deleteTodo={deleteTodo}
+          />
+        })}
+      </div>
     </div>
   );
 }
---
+++ fe/src/components/Todo/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as styles from './styles.module.css';
+
+export const Todo = ({id, text, completed, toggleComplete, deleteTodo}) => {
+    return (
+        <div className={styles.todo}>
+            <span className="material-icons" onClick={() => toggleComplete(id)}>
+                {completed ? 'check_box' : 'check_box_outline_blank'}
+            </span>
+            <p className={styles.text}>{text}</p>
+            <button className={styles.button} onClick={() => deleteTodo(id)}>
+                <span className="material-icons">delete</span>
+            </button>
+        </div>
+    )
+}
